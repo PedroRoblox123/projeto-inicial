0467522ceee157f2c4fe1a21f22f6a326a87fbfc
PedroRoblox123 <pedrosilva.szp@gmail.com>
2024-03-18 08:26:33 -0300

    formulário

--- scripts.js
+++ scripts.js
@@ -1,7 +1,36 @@
 var botao = document.getElementById("botao")
 botao.addEventListener("click", alertar);
 
+var nome = document.getElementById("nome");
+var email = document.getElementById("email");
+var telefone = document.getElementById("telefone");
+var cep = document.getElementById("cep");
+var logradouro = document.getElementById("logradouro");
+var numero = document.getElementById("numero");
+var complemento = document.getElementById("complemento");
+var bairro = document.getElementById("bairro");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
+
+
+var saida = document.getElementById("saida-de-dados") 
+
 function alertar(){
-    alert("O botão foi clicado!!!");
+    // alert("O nome inserido foi " + nome.value);
+
+    saida.innerText = "Nome:" + nome.value + 
+                    "\n Email:" + email.value +
+                    "\n Telefone:" + telefone.value +
+                    "\n CEP:" + cep.value +
+                    "\n Logradouro:" + logradouro.value +
+                    "\n Número:" + numero.value +
+                    "\n Complemento:" + complemento.value +
+                    "\n Bairro:" + bairro.value +
+                    "\n Cidade:" + cidade.value +
+                    "\n Estado:" + estado.value;
+
+
+
+
 }
 
